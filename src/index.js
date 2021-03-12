7c39e3f3f39d4d5ce8933a84768aedf8083160d3
Anastasiya Vashchonak <nvaschonok@gmail.com>
2021-03-12 23:59:35 +0300

    feat: fixed bugs

--- src/index.js
+++ src/index.js
@@ -1,26 +1,36 @@
 
 exports.min = function min (array) {
+  if (!array || array && array.length === 0) {
+    return 0
+  };
   let min = array[0];
   for(let i=1; i < array.length; i++){
     if (array[i] < min) min = array[i];
   }
-  return 0;
+  return min;
 }
 
 exports.max = function max (array) {
+  if (!array || array && array.length === 0) {
+    return 0
+  };
   let max = array[0];
   for(let i=1; i < array.length; i++){
     if (array[i] > max) max = array[i];
   }
-  return 0;
+  return max;
 }
 
 exports.avg = function avg (array) {
+  if (!array || array && array.length === 0) {
+    return 0
+  };
   let num = array.length;
   let sum = 0;
+  let avg;
   for(let i=0; i < num; i++){
       sum = sum + array[i];
   }
-  let avg = sum/num;
-  return 0;
+  avg = sum/num;
+  return avg;
 }
