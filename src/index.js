5e95261ca32f025a49d346a9b93ddfb8056f5180
Anastasiya Vashchonak <nvaschonok@gmail.com>
2021-03-12 23:08:08 +0300

    feat: added three programs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
+  let min = array[0];
+  for(let i=1; i < array.length; i++){
+    if (array[i] < min) min = array[i];
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  let max = array[0];
+  for(let i=1; i < array.length; i++){
+    if (array[i] > max) max = array[i];
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  let num = array.length;
+  let sum = 0;
+  for(let i=0; i < num; i++){
+      sum = sum + array[i];
+  }
+  let avg = sum/num;
   return 0;
 }
